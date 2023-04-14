515b2f1d18e7bab54f3f1d38da39579f50c7adc7
Xcaliburns <davidabruzzogames@gmail.com>
2023-04-14 08:53:13 +0200

    col1 experience

--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -1,47 +1,57 @@
-import React from 'react'
-import { useState } from 'react'
-import Navbar from '../Components/Navbar'
-import Photo from "../assets/photoMathieu.png"
+import React from "react";
+import { useState } from "react";
+import Navbar from "../Components/Navbar";
+import Photo from "../assets/photoMathieu.png";
 
 function Experience() {
-    const [gestionnaire,setGestionnaire]=useState(false);
-    const [formateur,setFormateur]=useState(false);
-    const [conducteur,setConducteur]=useState(false);
+  const [gestionnaire, setGestionnaire] = useState(false);
+  const [formateur, setFormateur] = useState(false);
+  const [conducteur, setConducteur] = useState(false);
 
-    const handleGestionnaire=()=>{
-      setGestionnaire(!gestionnaire)
-    }
+  const handleGestionnaire = () => {
+    setGestionnaire(!gestionnaire);
+  };
 
-    const handleFormateur=()=>{
-      setFormateur(!formateur)
-    }
+  const handleFormateur = () => {
+    setFormateur(!formateur);
+  };
 
-    const handleConducteur=()=>{
-      setConducteur(!conducteur)
-    }
+  const handleConducteur = () => {
+    setConducteur(!conducteur);
+  };
 
   return (
-   <div className='experience'><Navbar/> 
-   <div className='CV'>
-   <div className='col1'>    
-    <h1 className='name'>MathieuABRUZZO</h1>
-    <h2 className='fonction'> Enseignant de la conduite de la sécurité routière certifié BEPCASER</h2>
-</div>
-   <div className='col2'>
-    col2
-    <div >Diplomes et formations</div>
-    
-    <div >experience
-      <p onClick={handleGestionnaire}>gestionnaire de parc pour Forget formation</p>
-      <p onClick={handleFormateur}>Formateur Forget formation</p>
-      <p onClick={handleConducteur}>conducteur routier</p>
+    <div className="experience">
+      <Navbar />
+      <div className="CV">
+        <div className="col1">
+          <h1 className="name"><img className='photo'src={Photo} />Mathieu Abruzzo</h1>
+          <h2 className="fonction">
+            {" "}
+            <p>Enseignant de la conduite de la sécurité routière</p><p className="certif">certifié BEPCASER</p>
+          </h2>
+        </div>
+        <div className="col2">
+          col2
+          <div className="diplome">Diplomes et formations</div>
+          <div className="experrience">
+            experience
+            <p onClick={handleGestionnaire}>
+              gestionnaire de parc pour Forget formation
+            </p>
+            <div>
+              {gestionnaire && <p>le paragraphe gestionnaire de parc</p>}
+            </div>
+            <p onClick={handleFormateur}>Formateur Forget formation</p>
+            <div>{formateur && <p>le paragraphe formateur</p>}</div>
+            <p onClick={handleConducteur}>conducteur routier</p>
+            <div>{conducteur && <p>le paragraphe conducteur</p>}</div>
+          </div>
+          <div className="competence">compétences</div>
+        </div>
+      </div>
     </div>
-    <div>compétences</div>
-    </div>
-    </div>
-
-    </div>
-  )
+  );
 }
 
 export default Experience;
