ede5d3c55191b4c02b8acd22882b9f6ea801a1cc
Xcaliburns <davidabruzzogames@gmail.com>
2023-04-17 17:42:07 +0200

    form is ok I just need to hide dotenv

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React, { useRef } from "react";
+import Navbar from "../Components/Navbar";
+import emailjs from "@emailjs/browser";
+
+const serviceId = import.meta.env.SERVICE_ID;
+const templateId = import.meta.env.TEMPLATE_ID;
+const publicKey = import.meta.env.PUBLIC_KEY;
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
+      (result) => {
+        
+        console.log(result.text);
+        console.log("message envoyé");
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
+console.log(typeof(publicKey));
+console.log(templateId);
+console.log(serviceId);
   return (
-   <><Navbar/> <div>Contact</div></>
-  )
+    <div>
+      <Navbar />{" "}
+      <div className="contact">
+       
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+
+          <label>Email</label>
+          <input type="email" name="user_email" />
+
+          <label>accueil et déroulement de la formation </label>
+          <input type="number" name="note_accueil" min={1} max={5} />
+          <label>Commentaire</label>
+          <textarea name="com_accueil" />
+
+          <label>locaux et infrastructures </label>
+          <input type="number" name="note_infrastructures" min={1} max={5} />
+          <label>Commentaire</label>
+          <textarea name="com_locaux" />
+
+          <label>contenu formation </label>
+          <input type="number" name="note_contenu" min={1} max={5} />
+          <label>Commentaire</label>
+          <textarea name="com_contenu" />
+
+          <label>moyens mis en oeuvre </label>
+          <input type="number" name="note_moyens" min={1} max={5} />
+          <label>Commentaire</label>
+          <textarea name="com_moyens" />
+
+          <label>Enseignant </label>
+          <input type="number" name="note_enseignant" min={1} max={5} />
+          <label>Commentaire</label>
+          <textarea name="com_enseignant" />
+
+          <label>Commentaire sur la formation</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
